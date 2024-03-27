94ab92526aeed646573a90b787bec5667ee9d5e7
KunalGupta <g.kunal9@gmail.com>
2024-03-27 14:17:31 +0530

    section22-template refs

--- model-project/src/App.vue
+++ model-project/src/App.vue
@@ -1,18 +1,27 @@
 <template>
-  <h1>{{title}}</h1>
+  <h1>{{ title }}</h1>
+ <br>
+
+  <input type="text" ref="name" />
+  <button @click="handleClick">click me</button>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-  data(){
-    return{
-      title:"my first vue app :)"
-    }
-  }
-
-}
+  name: "App",
+  data() {
+    return {
+      title: "my first vue app :)",
+    };
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add("active")
+      this.$refs.name.focus()
+    },
+  },
+};
 </script>
 
 <style>
@@ -23,5 +32,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
